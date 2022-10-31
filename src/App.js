9b324795d5647da5a61a72a84c657222c183766d
unknown <sthitadhi2000@gmail.com>
2022-10-31 18:33:54 -0500

    a whole bunch of changes yalll

--- src/App.js
+++ src/App.js
@@ -7,24 +7,35 @@ import NavScrollExample from './Navbar';
 import { Row, Col } from 'react-bootstrap';  
 
 function ShowProperties ({ properties, filterText}) {
-    return(
+
+  const propertyItems = [];
+  
+  properties.map((property, index) => {
+    if (property.title.toLowerCase().includes(filterText.toLowerCase()) || property.location.city.toLowerCase().includes(filterText.toLowerCase()) || property.location.state.toLowerCase().includes(filterText.toLowerCase()) ) {
+      propertyItems.push(
+        <Col className="col-6" key={index}>
+        <PropertyCard property={property} key={index} />
+        </Col>
+      )
+    }
+
+    // if (property.title.toLowerCase()
+    // .indexOf(filterText.toLowerCase()) === -1 
+    // || property.location.city.toLowerCase()
+    // .indexOf(filterText.toLowerCase()) === -1) 
+    // {
+    //   return;
+    // }
+
+    
+  })
+
+    return(   
       <>
       <Row>
-      {properties.map((property, index) => {
-
-        if (property.title.toLowerCase().indexOf(filterText.toLowerCase()) === -1 || property.location.city.toLowerCase().indexOf(filterText.toLowerCase()) === -1) 
-        {
-          return;
-        }
-
-        return (
-          <Col className="col-6" key={index}>
-          <PropertyCard property={property} key={index} />
-          </Col>
-        )
-      })}
+      {propertyItems}
       </Row>
-    </>
+      </>
     );
   }
 
