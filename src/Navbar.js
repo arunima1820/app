dbe280285700e7cbabce22899120836660f46aee
unknown <sthitadhi2000@gmail.com>
2022-10-31 21:08:57 -0500

    Made style changes to NavBar

--- src/Navbar.js
+++ src/Navbar.js
@@ -12,9 +12,12 @@ function NavScrollExample( {onFilterTextChange}) {
   return (
     <Navbar bg="light" expand="lg">
       <Container fluid>
-        <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+      <a class="navbar-brand" href="#"><img src="./a.png" class="img-fluid" alt="Responsive image"
+                    id="logo" width="40"/></a>
+        <Navbar.Brand href="#">JSA Travels</Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
+        
           <Nav
             className="me-auto my-2 my-lg-0"
             style={{ maxHeight: '100px' }}
@@ -22,19 +25,8 @@ function NavScrollExample( {onFilterTextChange}) {
           >
             <Nav.Link href="#action1">Home</Nav.Link>
             <Nav.Link href="#action2">Link</Nav.Link>
-            <NavDropdown title="Link" id="navbarScrollingDropdown">
-              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action4">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action5">
-                Something else here
-              </NavDropdown.Item>
-            </NavDropdown>
-            <Nav.Link href="#" disabled>
-              Link
-            </Nav.Link>
+            
+          
           </Nav>
           <Form className="d-flex">
             <Form.Control
@@ -44,8 +36,8 @@ function NavScrollExample( {onFilterTextChange}) {
               aria-label="Search"
             
               onChange={(e) => onFilterTextChange(e.target.value)}
+              
             />
-            <Button variant="outline-success">Search</Button>
           </Form>
         </Navbar.Collapse>
       </Container>
