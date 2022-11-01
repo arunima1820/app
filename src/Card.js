4ee6945eecec110448627b5b0896fe8bfec9b22e
Arunima Mittra <arunima1820@gmail.com>
2022-10-31 19:57:40 -0500

    updated the card

--- src/Card.js
+++ src/Card.js
@@ -3,8 +3,14 @@ import Card from 'react-bootstrap/Card';
 import { useState } from 'react';
 import { Button, Col } from 'react-bootstrap';
 
-export default function PropertyCard(props) {
-  const [property, setProperty] = useState(props.property)
+export default function PropertyCard({property, isSelectedProperty, setSelectedProperty, setIsSelectedProperty}) {
+  const [property, setProperty] = useState(property)
+  const [expanded, setExpanded] = useState(isSelectedProperty)
+
+  const handleChange = function(e) {
+    setSelectedProperty(property)
+    setIsSelectedProperty(!isSelectedProperty)
+  }
 
   return (
     <Col className="container-fluid">
@@ -15,12 +21,29 @@ export default function PropertyCard(props) {
         <Card.Subtitle>{property.pricePerNight} per night</Card.Subtitle>
       </Card.Body>
       <Card.Body>
-        <Button href="#">
+      {expanded && 
+      <>
+      <Card.Text>
+          {property.description}
+        </Card.Text>
+        <ListGroup className="list-group-flush">
+        <ListGroup.Item>Service Fee: {property.serviceFee}</ListGroup.Item>
+        <ListGroup.Item>Cleaning Fee: {property.cleaningFee}</ListGroup.Item>
+        <ListGroup.Item><img src="./bed.svg" />{property.beds}&nbsp;<img src="./bath.svg"/>{property.bathrooms}</ListGroup.Item>
+        <ListGroup.Item><img src="./amenities.svg" />{property.amenities}</ListGroup.Item>
+      </ListGroup>
+        </>}
+
+
+        {!expanded && <Button onClick={(e) => handleChange(e)}>
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info-square-fill" viewBox="0 0 16 16">
   <path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm8.93 4.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM8 5.5a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"/>
 </svg>
  &nbsp; More info
-        </Button>
+        </Button>}
+        {expanded && <Button onClick={(e) => handleChange(e)}>
+        Back
+        </Button>}
       </Card.Body>
     </Card>
     </Col>
